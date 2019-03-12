38c70e04876294e86871e14449a70464b56f288d
sinol1978 <sinol1978@gmail.com>
2019-03-12 13:24:26 +0200

    add lesson07 Task03 finished

--- src/lesson04/Task4_DoubleParse/MainApp.java
+++ src/lesson04/Task4_DoubleParse/MainApp.java
@@ -5,6 +5,7 @@ import lesson07.Task03.Matrix;
 public class MainApp {
     public static void main(String[] args) {
         Matrix matrix = new Matrix(3, 2);
-        matrix.getMatrix();
+        matrix.printMatrix();
+
     }
 }
--- src/lesson07/Task03/Matrix.java
+++ src/lesson07/Task03/Matrix.java
@@ -3,6 +3,16 @@ package lesson07.Task03;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * Создать класс "Матрица". Класс должен иметь следующие поля:
+ * 1) двумерный массив вещественных чисел;
+ * 2) количество строк и столбцов в матрице.
+ * Класс должен иметь следующие методы:
+ * 1) сложение с другой матрицей;
+ * 2) умножение на число;
+ * 3) вывод на печать;
+ * 4) умножение матриц - по желанию.
+ */
 public class Matrix {
 
     private double[][] array;
@@ -17,19 +27,43 @@ public class Matrix {
         this.rows = rows;
         this.columns = columns;
         array = new double[rows][columns];
-        for(int i = 0; i<rows; i++){
-            for(int j=0; j<columns; j++){
-                array[i][j] = Math.random()*100;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                array[i][j] = (double) Math.round(Math.random() * 10000) / 100;
             }
         }
     }
 
-    public void getMatrix(){
-        for(int i = 0; i<rows; i++){
-            for(int j = 0; j<columns; j++){
+    public void printMatrix() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
                 System.out.print(array[i][j] + "\t");
             }
             System.out.println();
         }
     }
+
+    public Matrix plusMatrix(Matrix matrix) {
+        if (this.rows == matrix.rows && this.columns == matrix.columns) {
+            Matrix result = new Matrix(this.rows, this.columns);
+            for (int i = 0; i < this.rows; i++) {
+                for (int j = 0; j < this.columns; j++) {
+                    result.array[i][j] = this.array[i][j] + matrix.array[i][j];
+                }
+            }
+            return result;
+        }
+        return new Matrix(0, 0);
+    }
+
+    public Matrix multNumber(int n) {
+        Matrix result = new Matrix(this.rows, this.columns);
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
+                result.array[i][j] = this.array[i][j] * (double) n;
+            }
+        }
+        return result;
+    }
+
 }
