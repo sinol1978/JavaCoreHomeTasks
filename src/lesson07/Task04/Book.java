90d98fe2bc52d7435c0de8726a358f0543fbf732
sinol1978 <sinol1978@gmail.com>
2019-03-13 12:10:20 +0200

    add lesson07 Task04

--- src/lesson07/Task04/Book.java
+++ src/lesson07/Task04/Book.java
@@ -1,6 +1,26 @@
 package lesson07.Task04;
 
 public class Book {
-    String name;
-    String author;
+    private String name;
+    private String author;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+    public Book(String name, String author){
+        setName(name);
+        setAuthor(author);
+    }
 }
--- src/lesson07/Task04/Main.java
+++ src/lesson07/Task04/Main.java
@@ -2,7 +2,21 @@ package lesson07.Task04;
 
 public class Main {
     public static void main(String[] args) {
-        Reader r = new Reader();
+        Reader r = new Reader("John Smith");
         r.takeBook(1);
+
+        Book b1 = new Book("Cook Book", "Hannibal Lector");
+        Book b2 = new Book("Pattern MVC", "Vitaliy Klichko");
+
+        Book[] books = new Book[2];
+        books[0] = b1;
+        books[1] = b2;
+
+        r.takeBook(books);
+
+        r.returnBook(2);
+
+        r.returnBook(books);
+
     }
 }
