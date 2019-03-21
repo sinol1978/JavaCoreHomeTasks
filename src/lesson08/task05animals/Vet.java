203e9bf408eb5cf9d5104519da711bd2237e95df
sinol1978 <sinol1978@gmail.com>
2019-03-21 09:32:32 +0200

    1

--- src/lesson08/task05animals/Vet.java
+++ src/lesson08/task05animals/Vet.java
@@ -2,9 +2,6 @@ package lesson08.task05animals;
 
 public class Vet {
 
-    public Vet() {
-    }
-
     public static void treatAnimal(Animal animal) {
         System.out.print("Call from " + animal.getLocation() + ": ");
         System.out.println("Problems with " + animal.getFood());
