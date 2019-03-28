b293afd124d39592b36445f840bd680a0e371587
sinol1978 <sinol1978@gmail.com>
2019-03-28 08:39:53 +0200

    main

---
+++ src/lesson12/task03/Main.java
@@ -0,0 +1,4 @@
+package lesson12.task03;
+
+public class Main {
+}
---
+++ src/lesson12/task04/Main.java
@@ -0,0 +1,13 @@
+package lesson12.task04;
+
+/**
+ * Напишите метод, заменяющий в строке каждое второе вхождение «object-oriented programming» (не учитываем регистр символов) на «OOP».
+ * Например строка "Object-oriented programming is a programming language model organized
+ * around objects rather than "actions" and data rather than logic. Object-oriented programming blabla.
+ * Object-oriented programming bla."
+ * должна быть преобразована в "Object-oriented programming is a programming language model organized
+ * around objects rather than "actions" and data rather than logic. OOP blabla. Object-oriented programming bla."
+ */
+public class Main {
+
+}
