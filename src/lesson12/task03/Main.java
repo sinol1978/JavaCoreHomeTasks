990618154ce3f00ee05e6a07922167814522aecf
sinol1978 <sinol1978@gmail.com>
2019-03-29 22:57:42 +0200

    task03 started

--- src/lesson12/task03/Main.java
+++ src/lesson12/task03/Main.java
@@ -1,4 +1,5 @@
 package lesson12.task03;
+
 /**
  * Напишите метод, заменяющий в строке каждое второе вхождение «object-oriented programming» (не учитываем регистр символов) на «OOP».
  * Например строка "Object-oriented programming is a programming language model organized
@@ -9,4 +10,23 @@ package lesson12.task03;
  */
 
 public class Main {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Object-oriented programming is a programming language model organized around objects rather than \"actions\" and data rather than logic. Object-oriented programming blabla. Object-oriented programming bla.");
+        System.out.println(stringBuilder);
+        System.out.println("\n");
+        System.out.println(reverseOOP(stringBuilder));
+    }
+
+    private static StringBuilder reverseOOP(StringBuilder stringBuilder) {
+
+        return stringBuilder;
+    }
+
+    private static String reverseOOP(String string) {
+
+
+        return string;
+    }
+
 }
