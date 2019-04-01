c8231b9c68161ec14a9b33e9c42a209775bfb8f2
sinol1978 <sinol1978@gmail.com>
2019-04-01 13:54:40 +0300

    minCharCount

---
+++ src/lesson12/task11minlengthword/Main.java
@@ -0,0 +1,32 @@
+package lesson12.task11minlengthword;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+/**
+ * Найти слово, в котором число различных символов минимально.
+ * Слово может содержать буквы и цифры.
+ * Если таких слов несколько, найти первое из них.
+ * Например в строке "fffff ab f 1234 jkjk" найденное слово должно быть "fffff"
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        findMin("fffff ab f 1234 jkjk");
+
+    }
+
+    private static void findMin(String string) {
+        String[] words = string.split("\\W+");
+        for (String item : words) {
+            int count = 0;
+            for(int i = 0; i < item.length(); i++){
+                if(item.toCharArray()[i] == item.toCharArray()[i++]){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+        System.out.println(Arrays.toString(words));
+    }
+}
