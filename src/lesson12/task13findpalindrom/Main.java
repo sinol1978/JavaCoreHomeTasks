5b6a80849edcd282ffe4591d4f7ec82cba70b88d
sinol1978 <sinol1978@gmail.com>
2019-04-01 19:20:02 +0300

    13 finished 1

--- src/lesson12/task13findpalindrom/Main.java
+++ src/lesson12/task13findpalindrom/Main.java
@@ -1,7 +1,6 @@
 package lesson12.task13findpalindrom;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.*;
 
 public class Main {
     public static void main(String[] args) {
