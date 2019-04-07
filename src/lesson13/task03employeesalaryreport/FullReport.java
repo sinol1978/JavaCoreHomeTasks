ed6c6131d442ec09127cbb9f9dd78d481c86642c
sinol1978 <sinol1978@gmail.com>
2019-04-07 09:13:58 +0300

    13 finished 1

--- src/lesson13/task03employeesalaryreport/FullReport.java
+++ src/lesson13/task03employeesalaryreport/FullReport.java
@@ -7,7 +7,7 @@ public class FullReport extends Report {
     public void generateSalaryReport(Employee[] employees) {
         Locale current = new Locale("ru", "RU");
         ResourceBundle rb = ResourceBundle.getBundle("caption", current);
-        System.out.println(String.format(current, "%-20s%10s%15s", rb.getString("name"), rb.getString("salary"), rb.getString("date")));
+        System.out.println(String.format("%-20s%10s%15s", rb.getString("name"), rb.getString("salary"), rb.getString("date")));
         System.out.println("---------------------------------------------");
         for (Employee item : employees) {
             System.out.println(item + "     " + item.getSalaryDate());
