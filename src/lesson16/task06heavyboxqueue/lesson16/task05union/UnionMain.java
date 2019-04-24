e8c46b66107b7ff21141d6f8fcec19d81b4f70a6
sinol_ua <sinol1978@gmail.com>
2019-04-24 16:47:10 +0300

    lesson16 task05

---
+++ src/lesson16/task06heavyboxqueue/lesson16/task05union/UnionMain.java
@@ -0,0 +1,12 @@
+package lesson16.task06heavyboxqueue.lesson16.task05union;
+
+/**
+ * Напишите методы union(Set<?>... set) и intersect(Set<?> ... set),
+ * реализующих операции объединения и пересечения множеств.
+ * Протестируйте работу этих методов на предварительно заполненных множествах.
+ */
+public class UnionMain {
+    public static void main(String[] args) {
+
+    }
+}
