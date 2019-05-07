5b8d80e560e5f36044ad38ae6ad14b2f32d410c3
sinol_ua <sinol1978@gmail.com>
2019-05-07 21:01:41 +0300

    FileInputOutputStreamDemo

---
+++ src/lesson18/task02fileiodemo/FileInputOutputStreamDemo.java
@@ -0,0 +1,26 @@
+package lesson18.task02fileiodemo;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+public class FileInputOutputStreamDemo {
+    public static void main(String[] args) throws IOException {
+        try (OutputStream output = new FileOutputStream("src/io/file.txt");
+             InputStream input = new FileInputStream("src/io/file.txt")) {
+            char[] symbols = {'a', 'b', 'c'};
+            for (char i : symbols) {
+                output.write(i);
+            }
+
+            int size = input.available();
+            for (byte i : input.readAllBytes()) {
+                System.out.println((char) i + " ");
+            }
+        } catch (IOException e) {
+            System.out.println("Exception:\n" + e.getMessage());
+        }
+    }
+}
